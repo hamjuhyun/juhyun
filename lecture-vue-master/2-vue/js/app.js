dcf98c6bc2324e01d025a20388b924f9946ccaef
jucandoit <ods630805@empas.com>
2019-04-01 23:25:03 +0900

    뷰 Vue js

--- lecture-vue-master/2-vue/js/app.js
+++ lecture-vue-master/2-vue/js/app.js
@@ -1,66 +1,37 @@
 import SearchModel from './models/SearchModel.js'
-import KeywordModel from './models/KeywordModel.js'
-import HistoryModel from './models/HistoryModel.js'
 
 new Vue({
   el: '#app',
-  data: {
+  data : {
     query: '',
     submitted: false,
-    tabs: ['추천 검색어', '최근 검색어'],
+    tabs: ['추천검색어', '최근검색어'],
     selectedTab: '',
-    keywords: [],
-    history: [],
     searchResult: []
   },
   created() {
     this.selectedTab = this.tabs[0]
-    this.fetchKeyword()
-    this.fetchHistory()
   },
   methods: {
-    onSubmit(e) {
-      this.search()
+    onSubmit() {
+      this.search();
     },
-    onKeyup(e) {
-      if (!this.query.length) this.resetForm()
+    onReset() {
+      this.query = '';
+      this.submitted = false,
+      this.searchResult = []
     },
-    onReset(e) {
-      this.resetForm()
-    },
-    onClickTab(tab) {
-      this.selectedTab = tab
-    },
-    onClickKeyword(keyword) {
-      this.query = keyword;
-      this.search()
-    },
-    onClickRemoveHistory(keyword) {
-      HistoryModel.remove(keyword)
-      this.fetchHistory()
-    },
-    fetchKeyword() {
-      KeywordModel.list().then(data => {
-        this.keywords = data
-      })
-    },
-    fetchHistory() {
-      HistoryModel.list().then(data => {
-        this.history = data
-      })
+    onKeyup() {
+      if (!this.query.length) this.onReset();
     },
     search() {
       SearchModel.list().then(data => {
-        this.submitted = true
+        this.submitted = true,
         this.searchResult = data
       })
-      HistoryModel.add(this.query)
-      this.fetchHistory()
     },
-    resetForm() {
-      this.query = ''
-      this.submitted = false
-      this.searchResult = []
+    onClickTab(tab) {
+      this.selectedTab = tab
     }
   }
 })
