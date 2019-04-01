c2bffc3f9c4a9b77e31d92cbd7f9cff10f562c94
jucandoit <ods630805@empas.com>
2019-04-01 22:27:38 +0900

    탭 구현 #5

--- lecture-vue-master/1-vanilla/js/controllers/MainController.js
+++ lecture-vue-master/1-vanilla/js/controllers/MainController.js
@@ -1,5 +1,6 @@
 import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
+import TabView from '../views/TabView.js'
 
 import SearchModel from '../models/SearchModel.js'
 
@@ -12,7 +13,18 @@ export default {
             .on('@submit', e => this.onSubmit(e.input))
             .on('@reset', e => this.onResetForm())
 
+    TabView.setup(document.querySelector('#tabs'))
+           .on('@change', e => this.onChangeTab(e.detail.tabName))
+
     ResultView.setup(document.querySelector('#search-result'))
+
+    this.selectedTab = '추천 검색어'
+    this.renderView();
+  },
+
+  renderView() {
+    console.log(tag, 'renderView()')
+    TabView.setActiveTab(this.selectedTab);
   },
 
   search(query) {
@@ -36,5 +48,9 @@ export default {
 
   onSearchResult(data) {
     ResultView.render(data);
+  },
+
+  onChangeTab(tabName) {
+    // debugger;
   }
 }
